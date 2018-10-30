27dbdfb6171969c3f44cd4f845f540566d41b3fd
Ruben Branco <ruben.branco@outlook.pt>
2018-10-30 18:18:08 +0000

    Removing unnecessary index function

--- Projects/TPCs/TPC3/t3_fc50013.hs
+++ Projects/TPCs/TPC3/t3_fc50013.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import Data.List
 
 fromString :: String -> [(Char,Int)]
 fromString xs = map (\x -> (fst x, digitToInt$snd x))$filter (\x -> not (isDigit$fst x)) (zip xs (tail xs))
@@ -10,10 +11,7 @@ noteFrequency :: (Char, Int) -> Double
 noteFrequency (note, octave) = 440 * 2 ** (fromIntegral (midiCode (note, octave) - midiCode ('A', 4)) / fromIntegral 12)
 
 midiCode :: (Char, Int) -> Int
-midiCode (note, octave) = octave * 12 + (index note "CcDdEFfGgAaB") + 23
-
-index :: (Eq a) => a -> [a] -> Int
-index el xs = snd$head (filter (\x -> (fst x) == el) (zip xs [0..(length xs)]))
+midiCode (note, octave) = octave * 12 + head (note `elemIndices` "CcDdEFfGgAaB") + 23
 
 notesBelow :: Double -> [(Char,Int)] -> [(Char,Int)]
 notesBelow freq = filter (\x -> noteFrequency x < freq)
